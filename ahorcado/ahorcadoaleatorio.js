4686b34080254fe948bab785e6b22bc35dd66f14
Elliot <elliot.god@gmail.com>
2017-10-27 08:36:40 +0200

    Edición de la condicion del programa.

--- ahorcado/ahorcadoaleatorio.js
+++ ahorcado/ahorcadoaleatorio.js
@@ -1,9 +1,9 @@
-var aleatorio=Math.random()*(122-97)+97;
+﻿var aleatorio=Math.random()*(122-97)+97;
 aleatorio=String.fromCharCode(aleatorio);
 console.log(aleatorio);
 for(var i=0; i<5;i++){
   var letra=prompt("Introduce una letra de la a a la z en minúsculas.","");
-  if(letra<97 && letra>122){
+  if(letra<97 || letra>122){
     console.log("La letra no se encuentra dentro del rango permitido.");}
     else{
       if(letra==aleatorio){
